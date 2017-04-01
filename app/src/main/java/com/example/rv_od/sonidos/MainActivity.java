a6bc7424fec69fc6671bfb28a16188275c27b4de
OscarRodVi <oscar.daniel.rv@gmail.com>
2017-03-31 18:09:42 -0600

    Sonido v1.0

---
+++ app/src/main/java/com/example/rv_od/sonidos/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rv_od.sonidos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
